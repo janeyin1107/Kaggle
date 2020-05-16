06a2282aae49b22e99c1e45b8e70088e0404fb27
Jane Yin <jane.jingyuan.yin@gmail.com>
2020-05-16 12:55:31 +1000

    linplote

--- DataVisualization.py
+++ DataVisualization.py
@@ -4,3 +4,50 @@ import matplotlib.pyplot as plt
 %matplotlib inline
 import seaborn as sns
 print("Setup Complete")
+
+# Path of the file to read
+fifa_filepath = "../input/fifa.csv"
+
+# Read the file into a variable fifa_data
+fifa_data = pd.read_csv(fifa_filepath, index_col="Date", parse_dates=True)
+fifa_data.head()
+
+# Set the width and height of the figure
+plt.figure(figsize=(16,6))
+
+# Line chart showing how FIFA rankings evolved over time 
+sns.lineplot(data=fifa_data)
+
+# Path of the file to read
+spotify_filepath = "../input/spotify.csv"
+
+# Read the file into a variable spotify_data
+spotify_data = pd.read_csv(spotify_filepath, index_col="Date", parse_dates=True)
+
+# Set the width and height of the figure
+plt.figure(figsize=(14,6))
+
+# Add title
+plt.title("Daily Global Streams of Popular Songs in 2017-2018")
+
+# Line chart showing daily global streams of each song 
+sns.lineplot(data=spotify_data)
+
+list(spotify_data.columns)
+
+# Line chart showing daily global streams of 'Shape of You'
+sns.lineplot(data=spotify_data['Shape of You'], label="Shape of You")
+
+# Line chart showing daily global streams of 'Despacito'
+sns.lineplot(data=spotify_data['Despacito'], label="Despacito")
+
+# Add label for horizontal axis
+plt.xlabel("Date")
+
+
+
+
+
+
+
+
